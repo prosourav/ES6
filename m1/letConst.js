02de4e198d1ef8216d966e565ce166682aab94a6
prosourav <sourav19961010@gmail.com>
2021-02-01 00:47:06 +0530

    learnig let and const

--- m1/letConst.js
+++ m1/letConst.js
@@ -1,1 +1,23 @@
-console.log("Hello starting es6");
+const name ="sourav saha";
+// name = "rana roy"; (it wont work here)
+console.log(name);
+
+const numbers = [10, 12 , 43];
+numbers[2] = 34;
+numbers.push(22);
+// numbers = [10, 12, 34, 22];  it wont work as well
+console.log(numbers);
+
+const player = {name: "Ronaldo", phone: 4344};
+// player = {name: "Ronaldo", phone: 4344, Nationality: "Portugise"}; wont work
+console.log(player);
+
+let studentName = 'Sumit Roy';
+studentName = 'khokon Das';
+console.log(studentName);
+ let sum = 0;
+ for( let i = 0; i < 10; i++){
+ sum = sum + i;
+ }
+//  i wont work because let doesn't work outer scope
+ console.log(i);
